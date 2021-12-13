8483bcbd470b51d9b5a04da63b61b5942344bbec
Yu Ra Kim <yurakim.designs@gmail.com>
2021-12-13 15:32:02 -0500

    Project skeleton init commit, migration file for User table

---
+++ express-project-starter/db/migrations/20211213190112-create-user.js
@@ -0,0 +1,38 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        allowNull: false,
+        type: Sequelize.STRING(50),
+        unique: true
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING.BINARY
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING(250),
+        unique: true
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  }
+};
