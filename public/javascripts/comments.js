8190ee2fe0acbe1b71bb3ec3be964eef89dc6cbd
gitxandalf <alexandergangemi@outlook.com>
2021-12-16 13:12:35 -0800

    comments now display as expected

--- public/javascripts/comments.js
+++ public/javascripts/comments.js
@@ -1,32 +1,37 @@
-document.addEventListener("DOMContentLoaded", (event)=>{
-    const addCommentButton = document.querySelector(".commentSubmit");
-    addCommentButton.addEventListener("click", async (event) =>{
-        const commentTextarea = document.querySelector(".commentContent");
-        const storyId = commentTextarea.getAttribute("storyId");
-        const content = commentTextarea.value;
-        const body = {content, storyId};
-        try {
-            const commentJson = await fetch("/comments", {
-              method: "POST",
-              body: JSON.stringify(body),
-              headers: { "Content-Type": "application/json" },
-            });
-            const newComment = await commentJson.json();
-            const { comment, user } = newComment;
-            const commentUl = document.querySelector(".commentContainer");
-            // console.log(user)
-            const commentDiv = document.createElement("div");
-            commentDiv.innerHTML = `
-                  <p>${user}<p>
+document.addEventListener("DOMContentLoaded", (event) => {
+
+  const addCommentButton = document.querySelector(".commentSubmit");
+
+  addCommentButton.addEventListener("click", async (event) => {
+
+    const commentTextarea = document.querySelector(".commentContent");
+    const storyId = commentTextarea.getAttribute("storyId");
+    const content = commentTextarea.value;
+    const body = { content, storyId };
+
+    try {
+      const commentJson = await fetch("/comments", {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: { "Content-Type": "application/json" },
+      });
+      const newComment = await commentJson.json();
+      const { username, comment } = newComment;
+      const commentUl = document.querySelector(".commentContainer");
+      // console.log(user)
+      const commentDiv = document.createElement("div");
+      commentDiv.innerHTML = `
+                  <p>${username}<p>
                   <li>${comment.content}</li>
                   <button class='editComment'>Edit</button>
                   <button class='deleteComment'>Delete</button>
                   `;
 
-            commentUl.appendChild(commentDiv);
-          } catch (error) { }
-        console.log("zhen")
-        commentTextarea.value = "";
-    })
+      // commentUl.appendChild(commentDiv);
+      commentUl.insertBefore(commentDiv, commentUl.firstChild)
+    } catch (error) { }
+    console.log("zhen")
+    commentTextarea.value = "";
+  })
 
 })
